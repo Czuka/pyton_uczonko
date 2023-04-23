eb7e648ab182949b73b0232095440840241cbc57
Krzysztof zpaedzki <krzysztofzapedzki@gmail.com>
2023-04-23 19:11:06 +0200

    merge

--- sorting/merge_sort.py
+++ sorting/merge_sort.py
@@ -6,31 +6,68 @@ random_list_copy = []
 iterated_list = []
 iterated_list_copy = []
 
-for i in range(1000):
-    random_list.append(random.randint(1, 4000))
+for i in range(10000):
+    random_list.append(random.randint(1, 1000000))
 
-for i in range(1000):
-    iterated_list.append(1000-i)
+for i in range(10000):
+    iterated_list.append(10000-i)
 print(random_list)
 print(iterated_list)
 
 
 
-def merge(list):
-    for i in range (len(list)-1):
-        for j in range( len(list)-1-i):
-            if list[j] > list[j+1]:
-                list[j],list[j+1] = list[j+1], list[j]
-    return list
+def merge_sort(lst):
+    merge_sort_inplace(lst,0,len(lst)-1)
 
+def merge_sort_inplace(lst, start, end):
+    if start >= end:
+        return
+
+    mid = (start + end) // 2
+    merge_sort_inplace(lst, start, mid)
+    merge_sort_inplace(lst, mid + 1, end)
+    merge_inplace(lst, start, mid, end)
+
+
+def merge_inplace(lst, start, mid, end):
+    left = lst[start:mid + 1]
+    right = lst[mid + 1:end + 1]
+
+    i = j = 0
+    k = start
+
+    while i < len(left) and j < len(right):
+        if left[i] <= right[j]:
+            lst[k] = left[i]
+            i += 1
+        else:
+            lst[k] = right[j]
+            j += 1
+        k += 1
+
+    while i < len(left):
+        lst[k] = left[i]
+        i += 1
+        k += 1
+
+    while j < len(right):
+        lst[k] = right[j]
+        j += 1
+        k += 1
+
+
+
+
+random_list_copy = random_list.copy()
 start_time = time.time()
-bubbleSort(random_list)
+merge_sort(random_list_copy)
 end_time = time.time()
-print("iterated for:",random_list)
+print("random merge_sort :",random_list_copy)
 print("time: ", end_time-start_time )
 
+
 start_time = time.time()
-bubbleSort(iterated_list)
+merge_sort(iterated_list)
 end_time = time.time()
-print("iterated for:",iterated_list)
+print("iterated merge_sort:",iterated_list)
 print("time: ", end_time-start_time )
